6bbd965247fcd057c8c077e0fe2ef5f22c1813f6
Tahir H. Butt <tahir.butt@gmail.com>
2018-01-03 06:05:45 -0500

    Move to docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   devtool: 'inline-source-map',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'docs')
   },
   module: {
     rules: [
